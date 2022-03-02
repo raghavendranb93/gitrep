795c14330b2a5ee21f76e42b10822299044ee66d
raghavendranb93 <raghavendranbakale@gmail.com>
2022-03-02 11:20:54 +0530

    added a line

---
+++ basics.py
@@ -0,0 +1,10 @@
+a = "Ram"
+
+b = int(86)
+print(a, b)
+# default value
+print("My name is", a, "I am", b, "years old")
+print("My name is", a, "I am", b, "years old", end=" ", sep="@@")
+"a".isidentifier()
+
+
