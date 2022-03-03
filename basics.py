3958c826e918222cb3de4c86b894c9a45a937540
raghavendranb93 <raghavendranbakale@gmail.com>
2022-03-03 21:55:54 +0530

    changes made

--- basics.py
+++ basics.py
@@ -1,4 +1,4 @@
-a = "Ram"
+"""a = "Ram"
 
 b = int(86)
 print(a, b)
@@ -6,5 +6,12 @@ print(a, b)
 print("My name is", a, "I am", b, "years old")
 print("My name is", a, "I am", b, "years old", end=" ", sep="@@")
 "a".isidentifier()
+
+a = 10.0
+b = 2+3j"""
+
+line = "hi welcome python"
+print(line[-8:-15:-1])
+
 
 
---
+++ practice.py
@@ -0,0 +1,4 @@
+a = 20
+b = "hello"
+print(a, b)
+
